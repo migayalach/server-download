828e06a83a2a3cb4a95c6e7bb56768c0f7df98e4
migayalach <ayalachavezmiguel@gmail.com>
2025-07-23 12:38:27 -0400

    That component only require Type to returning promise

--- src/download/download.resolver.ts
+++ src/download/download.resolver.ts
@@ -1,35 +1,20 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { DownloadService } from './download.service';
-import { Download } from './entities/download.entity';
-import { CreateDownloadInput } from './dto/create-download.input';
-import { UpdateDownloadInput } from './dto/update-download.input';
+import { DownloadInput } from './dto/InputData.dto';
 
-@Resolver(() => Download)
+@Resolver('Download')
 export class DownloadResolver {
   constructor(private readonly downloadService: DownloadService) {}
 
-  @Mutation(() => Download)
-  createDownload(@Args('createDownloadInput') createDownloadInput: CreateDownloadInput) {
-    return this.downloadService.create(createDownloadInput);
+  @Mutation(() => String)
+  public async downloadingResourcer(
+    @Args('inputData') inputData: DownloadInput,
+  ): Promise<any> {
+    return await this.downloadService.downloading(inputData);
   }
 
-  @Query(() => [Download], { name: 'download' })
-  findAll() {
-    return this.downloadService.findAll();
-  }
-
-  @Query(() => Download, { name: 'download' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.downloadService.findOne(id);
-  }
-
-  @Mutation(() => Download)
-  updateDownload(@Args('updateDownloadInput') updateDownloadInput: UpdateDownloadInput) {
-    return this.downloadService.update(updateDownloadInput.id, updateDownloadInput);
-  }
-
-  @Mutation(() => Download)
-  removeDownload(@Args('id', { type: () => Int }) id: number) {
-    return this.downloadService.remove(id);
+  @Query(() => String)
+  hello(): string {
+    return 'Hello, you can download your favorite videos and musics from YouTube.';
   }
 }

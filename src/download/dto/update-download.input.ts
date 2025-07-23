802a40c9a2f24d4b8b925d330dbd99485864fa11
migayalach <ayalachavezmiguel@gmail.com>
2025-07-23 12:39:04 -0400

    deleted files

--- src/download/dto/update-download.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateDownloadInput } from './create-download.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateDownloadInput extends PartialType(CreateDownloadInput) {
-  @Field(() => Int)
-  id: number;
-}
